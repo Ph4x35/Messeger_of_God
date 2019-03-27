6e7e09e14ca78561528516833bcbc6f4a7de571f
Andre Luiz <kjnetsecurytsuport@gmail.com>
2019-03-27 15:21:17 -0300

    use python 2.7

---
+++ Mensageiro.py
@@ -0,0 +1,197 @@
+#! /user/bin/env/python
+# coding: utf-8
+#Codigo simples sinta se a vontade de modificalo
+#https://www.facebook.com/profile.php?id=100030877832029
+#irc.vircio.org #brasil nick:Phacker
+#use python2 programa.py
+ 
+try:
+    #imprime as opçoes de versiculos bíblicos
+    print ('MENSAGEIRO DE DEUS v1.0\n\nDE GRAÇA DEIS DE GRAÇA RECEBEIS\n\nAmor(1), Encorajamento(2), Família(3), Gratidão a Deus(4),\nConforto(5), Amizade(6), Casal(7), Perdão(8), Confiança em Deus(9), \nOração(10), Batismo(11), Palavra(12), Alegria(13), Força(14),\nJovems(15), Livramento(16), Incentivo(17), Animo(18),\nSuperação(19), Crianças(20), Conforto(21), Sabedoria(22),\nVitoria(23), Sonhos(24), Aliança(25), Bênção(26), Fé(27) \n\n')
+    #variaveis contendo os versiculos
+    a = "Leia: 1 Coríntios 13:4-7, Cânticos 8:7, Colossenses 3:14, 1 João 4:8, 1 Coríntios 13:13\n\nDeus abençoe!"
+    b = "Leia: Deuteronômio 31:6, Josué 1:9, Salmos 27:14, Isaías 43:5\n\nDeus abeçoe!"
+    c = "Leia: Gênesis 1:28, Josué 24:15, Salmos 103:17-18, Salmos 127:3-5, Gênesis 2:24\n\nDeus abençoe! "
+    d = "Leia: 1 Crônicas 16:34, Salmos 75:1, Salmos 7:17, 2 Coríntios 4:15, 1 Tessalonicenses 5:18, Salmos 50:14, Efésios 5:20, 1 Timóteo 2:1, 2 Coríntios 9:11, 1 Timóteo 1:12, Colossenses 2:7, 1 Crônicas 16:8, Efésios 5:4, Filipenses 4:6, 1 Timóteo 4:4, Salmos 79:13, Apocalipse 11:17, Salmos 30:11-12, Salmos 100:4, Salmos 70:4, Colossenses 3:15, Jonas 2:9, 1 Timóteo 4:4-5, Salmos 69:30\n\nDeus abençoe!"
+    e = "Leia: Mateus 11:28-29, Isaías 61:1-3, Deuteronômio 31:8, 1 Coríntios 1:3-7, Salmos 30:5\n\nDeus Abençoe!"
+    f = "Leia: Provérbios 17:17, Provérbios 13:20, Provérbios 27:5-6, João 15:15, Provérbios 27:9-10, Provérbios 18:24, Eclesiastes 4:9-10, 1 Coríntios 15:33, Romanos 12:10, 1 Samuel 18:1-3, Provérbios 22:24-25, Rute 1:16-17\n\nDeus abençoe!"
+    g = "Leia: Gênesis 2:18, Mateus 19:6, 1 Coríntios 13:4-7, Efésios 5:28-29, Efésios 5:33, Colossenses 3:14, Provérbios 18:22, Eclesiastes 4:9-12, Deuteronômio 24:5, Malaquias 2:15\n\nDeus abençoe!"
+    h = "Leia: Salmos 86:5, Mateus 18:21-22, Lucas 17:3-4, Colossenses 3:13, 1 João 1:9, Mateus 6:14-15, Efésios 4:32, Lucas 6:37, Isaías 43:25, Efésios 1:7, Colossenses 1:13-14, Isaías 55:7\n\nDeus abençoe!"
+    i = "Leia: 2 Coríntios 3:4-6, Salmos 18:2, Salmos 91:2, Naum 1:7, Salmos 84:12, 2 Samuel 22:31, Isaías 12:2, Jó 13:15, salmos 7:1, Provérbios 30:5, Salmos 9:10, Salmos 22:4, Provérbios 3:5, Salmos 37:5, Salmos 56:3, Salmos 71:5, Isaías 26:3 1 Timóteo 4;10, Salmos 20:7, Salmos 32:10, Salmos 118:8"
+    j = "Leia: Salmos 4:3, Salmos 37:7, Salmos 65:2, Tiago 5:16, Salmos 66:18, Provérbios 28:9, Isaías 66:24, Mateus 6:7-8, Mateus 6:9-13, Mateus 7:11, Mateus 7:7-8, Mateus 21:22, Lucas 18:1-8, João 14:13-14, marcos 11:24, Romanos 8:26-27, 2 Coríntios 12:8-9, Efésios 3:20 Efésios 6:18, Filipenses 4:19, Filipenses 4:6, 1 Tessalonicenses 5:17, 1 Timóteo 2:8, Hebreus 4:16, Tiago 1:5-8"
+    k = "Leia: Mateus 28:18-20, Mateus 3:13, Atos dos Apóstolos 2:28, Marcos 16:15-16, João 3:5, 1 Coríntios 12:13, 1 Coríntios 1:13-17, Atos dos Apóstolos 19:5, Romanos 6:2-7, João 1:33, Atos dos Apóstolos 1:5"
+    l = "Leia: 2 Timóteo 3:16-17, Salmos 119:105, Hebreus 4:12, Mateus 4:4, Salmos 119;11, joão 17:17, Salmos 119:89, Provérbios 30:5, Lucas 4:4, jeremias 23:29, João 17;17, 2 Timoteo 3:16, josué 1:8, Salmos 33:4, jeremias 15:16, Efésios 6:16\n\nDeus abençoe!"
+    m = "Leia: Filipenses 4:4, João 15:11, Salmos 28:7, Salmos 51:12, Salmos 47:1, Neemias 8:10, Salmos 16:8-9, Tiago 1:2-3, Isaías 9:3, Eclesiastes 9:7, Provérbios 10:28, 1 Pedro 1:8-9, Romanos 15:13, Lucas 15:7, Mateus 13:44, Isaías 12:6, Provérbios 15:23, João 16:22, Salmos 30:5, Romanos 14:17, Hebreus 12:2, Isaías 61:10\n\nDeus abençoe!"
+    n = "Leia: Filipenses 4:13, Salmos 27:1, 1 Coríntios 16:13, Deuteronômio 31:6, Deuteronômio 20:4, Isaías 41:10, Mateus 11:28, 2 Coríntios 12:9-10, Isaías 40:31, Salmos 31:24, Josué 1:9, 2 Crônicas 15:7, 1 Samuel 30;6, Salmos 27:14, Salmos 28:7, Salmos 73:26, Salmos 59:17, Efésios 6:10, Isaías 40:29\n\nDeus abençoe!"
+    o = "Leia: 1 João 2:14, Salmos 119:9, Salmos 148:12-13, Lamentaçoes 3:27, Salmos 119:11, Eclesiastes 11:9, 1 Pedro 5:5-6, Efésios 6:1-2, jeremias 29:11-14, Provérbios 20:11, Eclesiastes 12:1-3, 1 Timóteo 4:12, Provérbios 23:19-22, Salmos 71:5, Josué 1:7-9, Romanos 13:13"
+    p = "Leia: Êxodo 22:23, 2 Crônicas 7:14, Jó 33:26, Salmos 34:17, Salmos 32:6, Salmos 91:15, Mateus 7:11, João 9:31, João 14:13-16, Tiago 4:8, Atos dos Apóstolos 22:16, Romanos 10:13, Salmos 68:20, Salmos 82:4, Isaías 43:13, Daniel 3:17-18, 1 Tessalonicenses 1:10, Salmos 56:13, Salmos 91:2-3, 1 Crônicas 11:14, Salmos 40:13, 2 Samuel 22:18\n\nDeus abençoe!"
+    q = "Leia: Deuteronômio 31:6, Romanos 8:31, 2 Coríntios 4:16-18, Hebreus 10:15, 2 Samuel 23:12, Salmos 3:6, Salmos 37:23-24, Isaías 40:31, Salmos 37:5, Mateus 19:26, Marcos 9:23, Filipenses 4:13, Romanos 5:3-5, 2 Timóteo 1:7, Apocalipse 2;10\n\nDeus abençoe!"
+    r = "Leia: jeremias 29:11, 2 Coríntios 4:16, Filipenses 1:6, Salmos 138:3, 2 Timóteo 1:7, Salmos 27:1, Filipenses 4:13, 1 Pedro 1:6, 1 Tessalonicenses 3:7, Salmos 37:39, 1 Tessalonicenses 5:11, Josué 24:16-17, Romanos 1:11-12\n\nDeus abençoe!"
+    s = "Leia: Deuteronômio 1:21, Deuteronômio 20:4, Salmos 44:6-8, Lucas 10:19, Isaías 26:3, Mateus 11:28, Marcos 14:38, Lucas 22:40, Lucas 22:46, João 16:33, Romanos 8:28, Romanos 8:35, Romanos 12:21, 1 Coríntios 6:15-20, 1 Coríntios 10:13, 1 Coríntios 15:57, 2 Coríntios 2:10-11, 2 Coríntios 2:14, Gálatas 5:16-17, Efésios 4:22-24, Efésios 4:26-27, Filipenses 4:13, Efésios 6:10-17, 2 Tessalonicenses 3:3, 1 Timóteo 6:9, 2 Timóteo 4:18, Hebreus 2:18, Hebreus 13:5, Tiago 1:12-15, Tiago 4:7-8, Tiago 4:10, 1 Pedro 5;8-10, 1 João 2:12-15, 1 João 3:8, 1 João 4:4, 1 João 5:3-5, Apocalipse 3:21, Apocalipse 12:11, Salmos 27:1\n\nDeus abençoe!"
+    t = "Leia: Provérbios 22:6, Isaías 54:13, Efésios 6:1-3, Provérbios 1:8-9, Mateus 18:3-5, Provérbios 20:11, Isaías 38:19, Provérbios 17:6, 1 João 2:12\n\nDeus abençoe!"
+    u = "Leia: 2 Coríntios 1:3-4, João 16:33, Marcos 10:29-30, Mateus 5:4, 2 Coríntios 1:5, Mateus 11:28, Filipenses 4:6, Hebreus 13:5, Romanos 12:2, 1 Pedro 4:19, Isaías 41:10, Salmos 119:50, jÓ 5:11, Salmos 147:3, Apocalipse 21:3-4\n\nDeus abençoe!"
+    v = "Leia: Provérbios 16:9, Salmos 37:4, Hebreus 12:5-6, Eclesiastes 9:10, Salmos 1:1-2, Tiago 4:14, Colossenses 3:16, Salmos 16:11, Miqueias 6:8, Mateus 6:34, Salmos 119:105, Eclesiastes 7:12, Romanos 8:28, Provérbios 27:1, Tiago 1:5, Eclesiastes 2:26, Colossenses 2:2-3, Provérbios 3:7, Colossenses 3:23-24, Efésios 3:20-21\n\nDeus abençoe!"
+    x = "Leia: 1 João 5:4, Romanos 8:37-39, 1 Coríntios 15:55-57, Deuteronômio 20:4, Salmos 44:3-4, Efésios 6:13, Romanos 8:31, Josué 10:8, Provérbios 21:31, Filipenses 4:13, Salmos 108:13, Provérbios 24:16, Salmos 91:7\n\nDeus abençoe!"
+    z = "Leia: Gênesis 40:8, Números 12:6, Joel 2:28, Atos dos Apóstolos 2:14-17, Salmos 126:1, Deuteronômio 13:1-4, jeremias 23:25-26, jeremias 23:32, Gênesis 41:15-16, Eclesiastes 5:7, Daniel 1:17, Daniel 2:26-28, Mateus 1:20-21, Mateus 27:19, Eclesiastes 5:3, Provérbios 16:3, Provérbios 19:21\n\nDeus abençoe!"
+    w = "Leia: Hebreus 8:6, 1 Coríntios 11:25, Salmos 105:7-9, Gênesis 17:7-11, Êxodo 19:5-6, 2 Reis 13:23, Deuteronômio 4:31, Salmos 89:34-37, Isaías 54:10, Deuteronômio 7:7-12, Hebreus 8:8-12, Atos dos Apóstolos 3:25-26, Hebreus 9:15\n\nDeus abençoe!"
+    y = "Leia: Números 6:24-26, jeremias 17:7-8, jeremias 29:11, Êxodo 23:25, Provérbios 16:3, Filipenses 4:19, Deuteronômio 30:16, Salmos 34:8, Salmos 31:19, Lucas 6:27-28, 1 Pedro 3:9, Salmos 23:1-2, Malaquias 3:10, Gálatas 5:22-23, Esdras 8:23, Salmos 20:1-4, Salmos 3:8, Salmos 29:11, Provérbios 10:22\n\nDeus abençoe!"
+    ab = "Leia: Romanos 10:17, 2 Coríntios 5:7, Efésios 2:8-9, Romanos 4:5, Hebreus 11:1, Mateus 17:20, Mateus 21:21, Hebreus 11:6, Mateus 15:28, Mateus 9:2, Tito 1:2, 1 Pedro 1:9, 1 João 5:4, 1 Coríntios 16:13, 1 Timóteo 6:11, 1 Pedro 5:9, 1 Tessalonicenses 3:7, 2 Timóteo 4:7, Efésios 6:16, Gálatas 5:5, Tiago 1:3, Gálatas 2:20, João 3:36\n\nDeus abençoe!"
+   
+    print"https://ccb-agape.blogspot.com/\nDesenvolvido por Andre Luiz CCB";
+    #pede para digitar uma opçao de tema bíblico
+    mensagem = int(raw_input('Digite o numero corespondete a opção acima: '));
+    if mensagem == 1:
+        print a
+    elif mensagem == 2:
+        print b
+    elif mensagem == 3:
+        print c
+    elif mensagem == 4:
+        print d
+    elif mensagem == 5:
+        print e
+    elif mensagem == 6:
+        print f
+    elif mensagem == 7:
+        print g
+    elif mensagem == 8:
+        print h
+    elif mensagem == 9:
+        print i
+    elif mensagem == 10:
+        print j
+    elif mensagem == 11:
+        print k
+    elif mensagem == 12:
+        print l
+    elif mensagem == 13:
+        print m#! /user/bin/env/python
+# coding: utf-8
+#Codigo simples sinta se a vontade de modificalo
+#https://www.facebook.com/profile.php?id=100030877832029
+#irc.vircio.org #brasil nick:Phacker
+#use python2 programa.py
+ 
+try:
+    #imprime as opçoes de versiculos bíblicos
+    print ('MENSAGEIRO DE DEUS v1.0\n\nDE GRAÇA DEIS DE GRAÇA RECEBEIS\n\nAmor(1), Encorajamento(2), Família(3), Gratidão a Deus(4),\nConforto(5), Amizade(6), Casal(7), Perdão(8), Confiança em Deus(9), \nOração(10), Batismo(11), Palavra(12), Alegria(13), Força(14),\nJovems(15), Livramento(16), Incentivo(17), Animo(18),\nSuperação(19), Crianças(20), Conforto(21), Sabedoria(22),\nVitoria(23), Sonhos(24), Aliança(25), Bênção(26), Fé(27) \n\n')
+    #variaveis contendo os versiculos
+    b = "Leia: Deuteronômio 31:6, Josué 1:9, Salmos 27:14, Isaías 43:5\n\nDeus abeçoe!"
+    c = "Leia: Gênesis 1:28, Josué 24:15, Salmos 103:17-18, Salmos 127:3-5, Gênesis 2:24\n\nDeus abençoe! "
+    e = "Leia: Mateus 11:28-29, Isaías 61:1-3, Deuteronômio 31:8, 1 Coríntios 1:3-7, Salmos 30:5\n\nDeus Abençoe!"
+    f = "Leia: Provérbios 17:17, Provérbios 13:20, Provérbios 27:5-6, João 15:15, Provérbios 27:9-10, Provérbios 18:24, Eclesiastes 4:9-10, 1 Coríntios 15:33, Romanos 12:10, 1 Samuel 18:1-3, Provérbios 22:24-25, Rute 1:16-17\n\nDeus abençoe!"
+    g = "Leia: Gênesis 2:18, Mateus 19:6, 1 Coríntios 13:4-7, Efésios 5:28-29, Efésios 5:33, Colossenses 3:14, Provérbios 18:22, Eclesiastes 4:9-12, Deuteronômio 24:5, Malaquias 2:15\n\nDeus abençoe!"
+    h = "Leia: Salmos 86:5, Mateus 18:21-22, Lucas 17:3-4, Colossenses 3:13, 1 João 1:9, Mateus 6:14-15, Efésios 4:32, Lucas 6:37, Isaías 43:25, Efésios 1:7, Colossenses 1:13-14, Isaías 55:7\n\nDeus abençoe!"
+    i = "Leia: 2 Coríntios 3:4-6, Salmos 18:2, Salmos 91:2, Naum 1:7, Salmos 84:12, 2 Samuel 22:31, Isaías 12:2, Jó 13:15, salmos 7:1, Provérbios 30:5, Salmos 9:10, Salmos 22:4, Provérbios 3:5, Salmos 37:5, Salmos 56:3, Salmos 71:5, Isaías 26:3 1 Timóteo 4;10, Salmos 20:7, Salmos 32:10, Salmos 118:8"
+    j = "Leia: Salmos 4:3, Salmos 37:7, Salmos 65:2, Tiago 5:16, Salmos 66:18, Provérbios 28:9, Isaías 66:24, Mateus 6:7-8, Mateus 6:9-13, Mateus 7:11, Mateus 7:7-8, Mateus 21:22, Lucas 18:1-8, João 14:13-14, marcos 11:24, Romanos 8:26-27, 2 Coríntios 12:8-9, Efésios 3:20 Efésios 6:18, Filipenses 4:19, Filipenses 4:6, 1 Tessalonicenses 5:17, 1 Timóteo 2:8, Hebreus 4:16, Tiago 1:5-8"
+    k = "Leia: Mateus 28:18-20, Mateus 3:13, Atos dos Apóstolos 2:28, Marcos 16:15-16, João 3:5, 1 Coríntios 12:13, 1 Coríntios 1:13-17, Atos dos Apóstolos 19:5, Romanos 6:2-7, João 1:33, Atos dos Apóstolos 1:5"
+    l = "Leia: 2 Timóteo 3:16-17, Salmos 119:105, Hebreus 4:12, Mateus 4:4, Salmos 119;11, joão 17:17, Salmos 119:89, Provérbios 30:5, Lucas 4:4, jeremias 23:29, João 17;17, 2 Timoteo 3:16, josué 1:8, Salmos 33:4, jeremias 15:16, Efésios 6:16\n\nDeus abençoe!"
+    m = "Leia: Filipenses 4:4, João 15:11, Salmos 28:7, Salmos 51:12, Salmos 47:1, Neemias 8:10, Salmos 16:8-9, Tiago 1:2-3, Isaías 9:3, Eclesiastes 9:7, Provérbios 10:28, 1 Pedro 1:8-9, Romanos 15:13, Lucas 15:7, Mateus 13:44, Isaías 12:6, Provérbios 15:23, João 16:22, Salmos 30:5, Romanos 14:17, Hebreus 12:2, Isaías 61:10\n\nDeus abençoe!"
+    n = "Leia: Filipenses 4:13, Salmos 27:1, 1 Coríntios 16:13, Deuteronômio 31:6, Deuteronômio 20:4, Isaías 41:10, Mateus 11:28, 2 Coríntios 12:9-10, Isaías 40:31, Salmos 31:24, Josué 1:9, 2 Crônicas 15:7, 1 Samuel 30;6, Salmos 27:14, Salmos 28:7, Salmos 73:26, Salmos 59:17, Efésios 6:10, Isaías 40:29\n\nDeus abençoe!"
+    o = "Leia: 1 João 2:14, Salmos 119:9, Salmos 148:12-13, Lamentaçoes 3:27, Salmos 119:11, Eclesiastes 11:9, 1 Pedro 5:5-6, Efésios 6:1-2, jeremias 29:11-14, Provérbios 20:11, Eclesiastes 12:1-3, 1 Timóteo 4:12, Provérbios 23:19-22, Salmos 71:5, Josué 1:7-9, Romanos 13:13"
+    p = "Leia: Êxodo 22:23, 2 Crônicas 7:14, Jó 33:26, Salmos 34:17, Salmos 32:6, Salmos 91:15, Mateus 7:11, João 9:31, João 14:13-16, Tiago 4:8, Atos dos Apóstolos 22:16, Romanos 10:13, Salmos 68:20, Salmos 82:4, Isaías 43:13, Daniel 3:17-18, 1 Tessalonicenses 1:10, Salmos 56:13, Salmos 91:2-3, 1 Crônicas 11:14, Salmos 40:13, 2 Samuel 22:18\n\nDeus abençoe!"
+    q = "Leia: Deuteronômio 31:6, Romanos 8:31, 2 Coríntios 4:16-18, Hebreus 10:15, 2 Samuel 23:12, Salmos 3:6, Salmos 37:23-24, Isaías 40:31, Salmos 37:5, Mateus 19:26, Marcos 9:23, Filipenses 4:13, Romanos 5:3-5, 2 Timóteo 1:7, Apocalipse 2;10\n\nDeus abençoe!"
+    r = "Leia: jeremias 29:11, 2 Coríntios 4:16, Filipenses 1:6, Salmos 138:3, 2 Timóteo 1:7, Salmos 27:1, Filipenses 4:13, 1 Pedro 1:6, 1 Tessalonicenses 3:7, Salmos 37:39, 1 Tessalonicenses 5:11, Josué 24:16-17, Romanos 1:11-12\n\nDeus abençoe!"
+    s = "Leia: Deuteronômio 1:21, Deuteronômio 20:4, Salmos 44:6-8, Lucas 10:19, Isaías 26:3, Mateus 11:28, Marcos 14:38, Lucas 22:40, Lucas 22:46, João 16:33, Romanos 8:28, Romanos 8:35, Romanos 12:21, 1 Coríntios 6:15-20, 1 Coríntios 10:13, 1 Coríntios 15:57, 2 Coríntios 2:10-11, 2 Coríntios 2:14, Gálatas 5:16-17, Efésios 4:22-24, Efésios 4:26-27, Filipenses 4:13, Efésios 6:10-17, 2 Tessalonicenses 3:3, 1 Timóteo 6:9, 2 Timóteo 4:18, Hebreus 2:18, Hebreus 13:5, Tiago 1:12-15, Tiago 4:7-8, Tiago 4:10, 1 Pedro 5;8-10, 1 João 2:12-15, 1 João 3:8, 1 João 4:4, 1 João 5:3-5, Apocalipse 3:21, Apocalipse 12:11, Salmos 27:1\n\nDeus abençoe!"
+    t = "Leia: Provérbios 22:6, Isaías 54:13, Efésios 6:1-3, Provérbios 1:8-9, Mateus 18:3-5, Provérbios 20:11, Isaías 38:19, Provérbios 17:6, 1 João 2:12\n\nDeus abençoe!"
+    u = "Leia: 2 Coríntios 1:3-4, João 16:33, Marcos 10:29-30, Mateus 5:4, 2 Coríntios 1:5, Mateus 11:28, Filipenses 4:6, Hebreus 13:5, Romanos 12:2, 1 Pedro 4:19, Isaías 41:10, Salmos 119:50, jÓ 5:11, Salmos 147:3, Apocalipse 21:3-4\n\nDeus abençoe!"
+    v = "Leia: Provérbios 16:9, Salmos 37:4, Hebreus 12:5-6, Eclesiastes 9:10, Salmos 1:1-2, Tiago 4:14, Colossenses 3:16, Salmos 16:11, Miqueias 6:8, Mateus 6:34, Salmos 119:105, Eclesiastes 7:12, Romanos 8:28, Provérbios 27:1, Tiago 1:5, Eclesiastes 2:26, Colossenses 2:2-3, Provérbios 3:7, Colossenses 3:23-24, Efésios 3:20-21\n\nDeus abençoe!"
+    x = "Leia: 1 João 5:4, Romanos 8:37-39, 1 Coríntios 15:55-57, Deuteronômio 20:4, Salmos 44:3-4, Efésios 6:13, Romanos 8:31, Josué 10:8, Provérbios 21:31, Filipenses 4:13, Salmos 108:13, Provérbios 24:16, Salmos 91:7\n\nDeus abençoe!"
+    z = "Leia: Gênesis 40:8, Números 12:6, Joel 2:28, Atos dos Apóstolos 2:14-17, Salmos 126:1, Deuteronômio 13:1-4, jeremias 23:25-26, jeremias 23:32, Gênesis 41:15-16, Eclesiastes 5:7, Daniel 1:17, Daniel 2:26-28, Mateus 1:20-21, Mateus 27:19, Eclesiastes 5:3, Provérbios 16:3, Provérbios 19:21\n\nDeus abençoe!"
+    w = "Leia: Hebreus 8:6, 1 Coríntios 11:25, Salmos 105:7-9, Gênesis 17:7-11, Êxodo 19:5-6, 2 Reis 13:23, Deuteronômio 4:31, Salmos 89:34-37, Isaías 54:10, Deuteronômio 7:7-12, Hebreus 8:8-12, Atos dos Apóstolos 3:25-26, Hebreus 9:15\n\nDeus abençoe!"
+    y = "Leia: Números 6:24-26, jeremias 17:7-8, jeremias 29:11, Êxodo 23:25, Provérbios 16:3, Filipenses 4:19, Deuteronômio 30:16, Salmos 34:8, Salmos 31:19, Lucas 6:27-28, 1 Pedro 3:9, Salmos 23:1-2, Malaquias 3:10, Gálatas 5:22-23, Esdras 8:23, Salmos 20:1-4, Salmos 3:8, Salmos 29:11, Provérbios 10:22\n\nDeus abençoe!"
+    ab = "Leia: Romanos 10:17, 2 Coríntios 5:7, Efésios 2:8-9, Romanos 4:5, Hebreus 11:1, Mateus 17:20, Mateus 21:21, Hebreus 11:6, Mateus 15:28, Mateus 9:2, Tito 1:2, 1 Pedro 1:9, 1 João 5:4, 1 Coríntios 16:13, 1 Timóteo 6:11, 1 Pedro 5:9, 1 Tessalonicenses 3:7, 2 Timóteo 4:7, Efésios 6:16, Gálatas 5:5, Tiago 1:3, Gálatas 2:20, João 3:36\n\nDeus abençoe!"
+   
+    print"Caso goste e deseje doar qualquer quantia : http://vaka.me/468135 \n não e obrigatorio!\n"
+    #pede para digitar uma opçao de tema bíblico
+    mensagem = int(raw_input('Digite o numero corespondete a opção acima: '))
+    if mensagem == 1:
+        print a
+    elif mensagem == 2:
+        print b
+    elif mensagem == 3:
+        print c
+    elif mensagem == 4:
+        print d
+    elif mensagem == 5:
+        print e
+    elif mensagem == 6:
+        print f
+    elif mensagem == 7:
+        print g
+    elif mensagem == 8:
+        print h
+    elif mensagem == 9:
+        print i
+    elif mensagem == 10:
+        print j
+    elif mensagem == 11:
+        print k
+    elif mensagem == 12:
+        print l
+    elif mensagem == 13:
+        print m
+    elif mensagem == 14:
+        print n
+    elif mensagem == 15:
+        print o
+    elif mensagem == 16:
+        print p
+    elif mensagem == 17:
+        print q
+    elif mensagem == 18:
+        print r
+    elif mensagem == 19:
+        print s
+    elif mensagem == 20:
+        print t
+    elif mensagem == 21:
+        print u
+    elif mensagem == 22:
+        print v
+    elif mensagem == 23:
+        print x
+    elif mensagem == 24:
+        print z
+    elif mensagem == 25:
+       print w
+    elif mensagem == 26:
+       print y
+    elif mensagem == 27:
+       print ab
+    else:
+       print 'O numero nao corresponde!'  
+except:
+    print'Voçê digitou um parametro incorreto!'
+    elif mensagem == 14:
+        print n
+    elif mensagem == 15:
+        print o
+    elif mensagem == 16:
+        print p
+    elif mensagem == 17:
+        print q
+    elif mensagem == 18:
+        print r
+    elif mensagem == 19:
+        print s
+    elif mensagem == 20:
+        print t
+    elif mensagem == 21:
+        print u
+    elif mensagem == 22:
+        print v
+    elif mensagem == 23:
+        print x
+    elif mensagem == 24:
+        print z
+    elif mensagem == 25:
+       print w
+    elif mensagem == 26:
+       print y
+    elif mensagem == 27:
+       print ab
+    else:
+       print 'O numero nao corresponde!'  
+except:
+    print'Voçê digitou um parametro incorreto!'
